d234b366f5201881e1c3695df3edf48f2e0d50d1
iemalteria <institucionmalteria@gmail.com>
2024-05-21 19:48:31 -0500

    Update BlogPage.js

--- client/src/pages/BlogPage.js
+++ client/src/pages/BlogPage.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 export default function BlogPage(){
     const [posts,setPosts] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:4000/post').then(response => {
+        fetch('http://https://iemalteria-of.vercel.app/post').then(response => {
             response.json().then(posts => {
                 setPosts(posts);
             });
