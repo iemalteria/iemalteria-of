81fc8fdcc52978a0913d8cfdf2eb2c80648a4b8a
iemalteria <institucionmalteria@gmail.com>
2024-05-21 19:51:54 -0500

    Update RegisterPage.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -18,7 +18,7 @@ export default function RegisterPage() {
             return;
         }
 
-        const response = await fetch('http://localhost:4000/register', {
+        const response = await fetch('http://iemalteria-of.vercel.app/register', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
