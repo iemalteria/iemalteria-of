487eb68ff9eaeda81bf397c1f7f088cd58a4d7e9
iemalteria <institucionmalteria@gmail.com>
2024-05-21 20:15:08 -0500

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -32,7 +32,7 @@ export default function CreatePost() {
         data.set('content', content);
         data.set('file', files[0]);
         ev.preventDefault();
-        const response = await fetch('http://iemalteria-of.vercel.app/post', {
+        const response = await fetch('https://iemalteria-of.vercel.app/post', {
             method: 'POST',
             body: data,
             credentials: 'include',
