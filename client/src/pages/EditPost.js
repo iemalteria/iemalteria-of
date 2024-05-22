5d00b8cc602deff0f71d14bb330d0a644e4370e3
iemalteria <institucionmalteria@gmail.com>
2024-05-21 19:49:17 -0500

    Update EditPost.js

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -30,7 +30,7 @@ export default function EditPost() {
     const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
-        fetch(`http://localhost:4000/post/${id}`)
+        fetch(`http://iemalteria-of.vercel.app/post/${id}`)
             .then(response => response.json())
             .then(postInfo => {
                 setTitle(postInfo.title);
